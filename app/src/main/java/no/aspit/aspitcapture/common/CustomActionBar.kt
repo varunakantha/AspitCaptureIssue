31a6c4383a14c395b24ad7c31161a395b418f2c0
sajith kumara <sku@tiqri.com>
2018-08-22 17:21:46 +0530

    12524 Android - UI - CustomActionbar optimized.

--- app/src/main/java/no/aspit/aspitcapture/common/CustomActionBar.kt
+++ app/src/main/java/no/aspit/aspitcapture/common/CustomActionBar.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.graphics.Color
 import android.util.AttributeSet
 import android.view.LayoutInflater
+import android.view.View
 import android.widget.LinearLayout
 import android.widget.Toast
 import kotlinx.android.synthetic.main.custom_action_bar.view.*
@@ -23,6 +24,7 @@ class CustomActionBar : LinearLayout {
         var mainTitleText: String? = "Main title"
         var subTitleText: String? = "Sub title"
         var mainBgColor = Color.DKGRAY
+        var pipeVisibility:Int = View.GONE
 
         val inflater = LayoutInflater.from(getContext())
         inflater.inflate(R.layout.custom_action_bar, this)
@@ -32,12 +34,14 @@ class CustomActionBar : LinearLayout {
             mainTitleText = array.getString(R.styleable.CustomActionBar_main_text)
             subTitleText = array.getString(R.styleable.CustomActionBar_sub_title)
             mainBgColor = array.getColor(R.styleable.CustomActionBar_bar_bg_color, mainBgColor)
+            pipeVisibility = array.getInt(R.styleable.CustomActionBar_pipe_visibility, pipeVisibility)
             array.recycle()
         }
 
         main_title.text = mainTitleText
         sub_title.text = subTitleText
         constraint_layout.setBackgroundColor(mainBgColor)
+        pipe_bar.visibility = pipeVisibility
         close_view.setOnClickListener {
             Toast.makeText(getContext(), "Button clicked", Toast.LENGTH_LONG).show()
             //                ((Activity)context).finish();
--- app/src/main/java/no/aspit/aspitcapture/ui/patientlookup/PatientLookUpActivity.kt
+++ app/src/main/java/no/aspit/aspitcapture/ui/patientlookup/PatientLookUpActivity.kt
@@ -1,25 +1,17 @@
 package no.aspit.aspitcapture.ui.patientlookup
 
-import android.graphics.Color
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_patient_look_up.*
-import kotlinx.android.synthetic.main.custom_action_bar.view.*
 import no.aspit.aspitcapture.R
-import no.aspit.aspitcapture.common.CustomActionBar
 
 class PatientLookUpActivity : AppCompatActivity() {
-    lateinit var actionBar : CustomActionBar
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_patient_look_up)
-        actionBar = custom_action_bar
-        actionBar.main_title.text = "Harald Scjierton"
-        actionBar.sub_title.text = "Physiotherapist"
-        actionBar.constraint_layout.setBackgroundColor(Color.TRANSPARENT)
 
         patient_ssn.addTextChangedListener(object : TextWatcher {
 
