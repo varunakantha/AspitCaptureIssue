cdedd86af16c5a5f40fa667bc255f8a6497489b9
pwe <pwe@gmail.com>
2018-08-23 15:20:37 +0530

    #12511 image details UI development

--- app/src/main/java/no/aspit/aspitcapture/ui/imagecapture/CapturedImageDetailsAddActivity.kt
+++ app/src/main/java/no/aspit/aspitcapture/ui/imagecapture/CapturedImageDetailsAddActivity.kt
@@ -1,11 +1,30 @@
 package no.aspit.aspitcapture.ui.imagecapture
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.TextView
+import kotlinx.android.synthetic.main.captured_image_add_details.*
+import no.aspit.aspitcapture.R
 import no.aspit.aspitcapture.common.BaseActivity
 
 class CapturedImageDetailsAddActivity : BaseActivity() {
+
+    lateinit var capturedImage: ImageView
+    lateinit var imageTitle: TextView
+    lateinit var imageTitleEdit: EditText
+    lateinit var imageCommentEdit: EditText
+    lateinit var confirmButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.captured_image_add_details)
+        capturedImage = captured_imageView
+        imageTitle = image_title_textView
+        imageTitleEdit = image_title_editText
+        imageCommentEdit = image_comment_editText
+        confirmButton = image_details_confirm_button
     }
 
 }
