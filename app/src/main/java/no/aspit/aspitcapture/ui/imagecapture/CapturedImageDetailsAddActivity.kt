11a17a59d9db3e90d30b562beae912f0672dc77b
pwe <pwe@gmail.com>
2018-08-23 12:38:57 +0530

    12511 initial

---
+++ app/src/main/java/no/aspit/aspitcapture/ui/imagecapture/CapturedImageDetailsAddActivity.kt
@@ -0,0 +1,11 @@
+package no.aspit.aspitcapture.ui.imagecapture
+
+import android.os.Bundle
+import no.aspit.aspitcapture.common.BaseActivity
+
+class CapturedImageDetailsAddActivity : BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+}
