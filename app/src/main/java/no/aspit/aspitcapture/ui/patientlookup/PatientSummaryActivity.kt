deaa645d730f72972ea869f786fbd528b6dfdc2d
sajith kumara <sku@tiqri.com>
2018-08-27 10:37:12 +0530

    12615 Android - patient details - add close interface to patient summary

--- app/src/main/java/no/aspit/aspitcapture/ui/patientlookup/PatientSummaryActivity.kt
+++ app/src/main/java/no/aspit/aspitcapture/ui/patientlookup/PatientSummaryActivity.kt
@@ -1,13 +1,18 @@
 package no.aspit.aspitcapture.ui.patientlookup
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import no.aspit.aspitcapture.R
+import no.aspit.aspitcapture.common.BaseActivity
+import no.aspit.aspitcapture.common.CustomActionBar
 
-class PatientSummaryActivity : AppCompatActivity() {
+class PatientSummaryActivity : BaseActivity(),CustomActionBar.CustomActionBarInterface {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_patient_summary)
+    }
+
+    override fun onClose() {
+        finish()
     }
 }
