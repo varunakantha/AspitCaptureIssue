18849b04f3e7fe15df81efc43033ddc19ddefebd
sajith kumara <sku@tiqri.com>
2018-08-27 11:15:18 +0530

    12615 Android - patient details - when user enter 5 character, automatically open the next screen. new images for scan and worklist.

--- app/src/main/java/no/aspit/aspitcapture/ui/patientlookup/PatientLookUpActivity.kt
+++ app/src/main/java/no/aspit/aspitcapture/ui/patientlookup/PatientLookUpActivity.kt
@@ -4,7 +4,6 @@ import android.content.Intent
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
-import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_patient_look_up.*
 import no.aspit.aspitcapture.R
 import no.aspit.aspitcapture.common.BaseActivity
@@ -26,19 +25,12 @@ class PatientLookUpActivity : BaseActivity(),CustomActionBar.CustomActionBarInte
 
             override fun onTextChanged(s: CharSequence, start: Int,
                                        before: Int, count: Int) {
-                if (s.length == 5)
-                    retrieved_patient_ssn.text = "12443324234"
+                if (s.length == 5) {
+                    val intent = Intent(this@PatientLookUpActivity, PatientSummaryActivity::class.java)
+                    startActivity(intent)
+                }
             }
         })
-
-        patient_card_view.setOnClickListener { v ->
-            if(retrieved_patient_ssn.text.length < 5)
-                Toast.makeText(this,"SSN length is too small. Please Check again.", Toast.LENGTH_LONG).show()
-            else {
-                val intent = Intent(this,PatientSummaryActivity::class.java)
-                startActivity(intent)
-            }
-        }
     }
 
     override fun onClose() {
