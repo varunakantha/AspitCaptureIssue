c5318ce722f5fc965f1314f8422eda800b494ee5
Sanath Nandasiri <sanathe06@gmail.com>
2018-09-05 13:49:10 +0530

    fixed authentication flow issues

--- app/src/main/java/no/aspit/capture/ui/patientlookup/PatientLookUpActivity.kt
+++ app/src/main/java/no/aspit/capture/ui/patientlookup/PatientLookUpActivity.kt
@@ -12,12 +12,12 @@ import no.aspit.capture.common.BaseActivity
 import no.aspit.capture.common.Constant.Companion.KEY_PATIENT_OBJECT
 import no.aspit.capture.common.Constant
 import no.aspit.capture.common.CustomActionBar
+import no.aspit.capture.extention.readString
 import no.aspit.capture.common.JsonParser
 import no.aspit.capture.common.Utils
 import no.aspit.capture.extention.saveString
 import no.aspit.capture.extention.toast
 import no.aspit.capture.net.Service
-import no.aspit.capture.extention.readString
 import no.aspit.capture.ui.startscreen.RegistrationActivity
 import retrofit2.Call
 import retrofit2.Callback
@@ -42,14 +42,17 @@ class PatientLookUpActivity : BaseActivity(), CustomActionBar.ActionBarListener 
             override fun onTextChanged(s: CharSequence, start: Int,
                                        before: Int, count: Int) {
                 if (s.length == SSN_LENGTH) {
+                    showProgress()
                     Service(this@PatientLookUpActivity,
                             Utils().getAccessToken(this@PatientLookUpActivity)!!.authToken)
                             .getPatientBySSN(s.toString(), object : Callback<Patient> {
                                 override fun onFailure(call: Call<Patient>?, t: Throwable?) {
+                                    hideProgress()
                                     toast("Cannot find a patient with that SSN")
                                 }
 
                                 override fun onResponse(call: Call<Patient>?, response: Response<Patient>?) {
+                                    hideProgress()
                                     if (response!!.isSuccessful) {
                                         response.body()?.firstName?.let { toast(it) }
                                         startActivity(Intent(this@PatientLookUpActivity, PatientSummaryActivity::class.java))
@@ -79,4 +82,5 @@ class PatientLookUpActivity : BaseActivity(), CustomActionBar.ActionBarListener 
         startActivity(intent)
         finish()
     }
+
 }
