ac2d7ed89a88f477c48d76779f91f59cc09a69f4
Sanath Nandasiri <sanathe06@gmail.com>
2018-09-10 15:06:24 +0530

    added app centre Crashes and Analytics

--- app/src/main/java/no/aspit/capture/AspitCaptureApplication.kt
+++ app/src/main/java/no/aspit/capture/AspitCaptureApplication.kt
@@ -4,6 +4,11 @@ import android.app.Application
 import com.facebook.stetho.Stetho
 import com.facebook.stetho.okhttp3.StethoInterceptor
 import okhttp3.OkHttpClient
+import com.microsoft.appcenter.crashes.Crashes
+import com.microsoft.appcenter.analytics.Analytics
+import com.microsoft.appcenter.AppCenter
+
+
 
 
 class AspitCaptureApplication : Application() {
@@ -15,5 +20,8 @@ class AspitCaptureApplication : Application() {
                     .addNetworkInterceptor(StethoInterceptor())
                     .build()
         }
+
+        AppCenter.start(this, "bf935cf6-4a9d-4f8f-90ca-25308f9cb9b3",
+                Analytics::class.java, Crashes::class.java)
     }
 }
